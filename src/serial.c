32cde63eb30aba1ff664f63c4d43639d75ff931b
Esa Varemo <esa@kuivanto.fi>
2014-04-05 15:48:30 +0300

    wtf formatting

--- src/serial.c
+++ src/serial.c
@@ -637,8 +637,8 @@ static void evaluateOtherData(uint8_t sr)
             cliProcess();
             break;
         case 'R':
-						if(!f.ARMED)
-						    systemReset(true);      // reboot to bootloader
+            if(!f.ARMED)
+                systemReset(true);      // reboot to bootloader
             break;
     }
 }
